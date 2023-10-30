45eddcd3f0c8c2fc3bbe46fb00c2959c8ffa2578
lotfi <lotfi.gheribi@quant.global>
2023-10-30 10:32:26 +0100

    :hammer: fork the repo to download it for Virtual tryon

--- u2net_load.py
+++ u2net_load.py
@@ -1,7 +1,7 @@
 import os
 import torch
-from model import U2NET # full size version 173.6 MB
-from model import U2NETP # small version u2net 4.7 MB
+from ACGPN.U_2_Net.model import U2NET # full size version 173.6 MB
+from ACGPN.U_2_Net.model import U2NETP # small version u2net 4.7 MB
 
 
 def model(model_name='u2net'):
@@ -15,7 +15,7 @@ def model(model_name='u2net'):
     elif(model_name=='u2netp'):
         print("...load U2NEP---4.7 MB")
         net = U2NETP(3,1)
-    net.load_state_dict(torch.load(model_dir))
+    net.load_state_dict(torch.load(model_dir, map_location={'cuda:0': 'cpu'}))
 
     if torch.cuda.is_available():
         net.cuda()
